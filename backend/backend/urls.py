795ee6d6d43686a473a954a6bf078a588216f4cf
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-10-21 17:32:59 +0900

    changed code structure for preprocess / modified to follow PEP-8

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('xmlParser/', include('xmlParser.urls')),
     path('admin/', admin.site.urls),
 ]
--- backend/xmlParser/urls.py
+++
@@ -1,7 +0,0 @@
-#I made this file
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.pill_list, name='pill_list'),
-]
