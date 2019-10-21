e8b806ffa8121da7a953924a9bf84cd7407810ff
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-10-21 17:41:22 +0900

    added requirements.txt

--- backend/dataset/preprocess.py
+++ backend/dataset/preprocess.py
@@ -4,22 +4,25 @@ import json
 from copy import deepcopy
 from tqdm import tqdm
 
-# parse the xml file -> Format it into json fixture format, but DO NOT include the unnecessary tags
+"""
+Parses the xml file, and saves into json fixture format (for Django Model), excluding unnecessary tags
+"""
+
 idx = 0
 jsonList = []
-jsonDictFormat = {  # You ALWAYS have to use this format 
+jsonDictFormat = {  # Django Model enforces this json format!
     "idx": idx,
     "model": "xmlParser.pill",
     "fields": {
-        "id" : idx,
-        "take_method" : "",
-        "product_name" : "",
-        "expiration_date" : "",
-        "functions" : "",
-        "store_method" : "",
-        "company_name" : "",
-        "standards" : "",
-        "precautions" : ""
+        "id": idx,
+        "take_method": "",
+        "product_name": "",
+        "expiration_date": "",
+        "functions": "",
+        "store_method": "",
+        "company_name": "",
+        "standards": "",
+        "precautions": ""
     }
 }
 
@@ -32,14 +35,14 @@ def parser(tree):
         global idx
         jsonDictFormat["pk"] = idx
         jsonDictFormat["fields"]["id"] = idx
-        jsonDictFormat["fields"]["take_method"] = root[2+i][0].text
-        jsonDictFormat["fields"]["product_name"] = root[2+i][1].text
-        jsonDictFormat["fields"]["expiration_date"] = root[2+i][4].text
-        jsonDictFormat["fields"]["functions"] = root[2+i][6].text
-        jsonDictFormat["fields"]["store_method"] = root[2+i][8].text
-        jsonDictFormat["fields"]["company_name"] = root[2+i][10].text
-        jsonDictFormat["fields"]["standards"] = root[2+i][13].text
-        jsonDictFormat["fields"]["precautions"] = root[2+i][14].text
+        jsonDictFormat["fields"]["take_method"] = root[2 + i][0].text
+        jsonDictFormat["fields"]["product_name"] = root[2 + i][1].text
+        jsonDictFormat["fields"]["expiration_date"] = root[2 + i][4].text
+        jsonDictFormat["fields"]["functions"] = root[2 + i][6].text
+        jsonDictFormat["fields"]["store_method"] = root[2 + i][8].text
+        jsonDictFormat["fields"]["company_name"] = root[2 + i][10].text
+        jsonDictFormat["fields"]["standards"] = root[2 + i][13].text
+        jsonDictFormat["fields"]["precautions"] = root[2 + i][14].text
         idx += 1
         jsonList.append(deepcopy(jsonDictFormat))
 
@@ -55,6 +58,6 @@ if __name__ == '__main__':
             tree = ElementTree.parse(fullname)
             parser(tree)
             print(filename)
-        break   # 모든 파일 다 하고 싶으면 이거 없애기! 일단 한 파일에 대해서만 테스트해 보았음.
+        break  # 모든 파일 다 하고 싶으면 이거 없애기! 일단 한 파일에 대해서만 테스트해 보았음.
     with open('./fixtures/pill_data.json', 'w', encoding='utf-8') as f:
         json.dump(jsonList, f, ensure_ascii=False, indent=4)
