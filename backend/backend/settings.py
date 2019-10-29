c5690f4bc755f888391ad26a13e68250da982614
jeehoonhyun <peterpan1998@snu.ac.kr>
2019-10-29 14:40:44 +0900

    Added login backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'pillbox.User'
 
 # Application definition
 
 INSTALLED_APPS = [
+    'pillbox.apps.PillboxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- backend/pillbox/urls.py
+++ backend/pillbox/urls.py
@@ -3,5 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('image/', views.image, name='image')
+    path('user/signin/', views.signin, name='login'),
+    path('user/signout/', views.signout, name='login'),
+    path('user/signup/', views.signup, name='signup'),
+    path('user/<int:id>/', views.userInfo, name='userInfo'),
+
+    path('token/', views.token, name='token'),
+
+
+    path('image/', views.image, name='image'),
 ]
