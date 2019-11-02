d189936adb0d68d069a6f42143185acc821cca7e
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-11-02 17:45:14 +0900

    back trolling reverted

--- backend/pill/views.py
+++ backend/pill/views.py
@@ -1,22 +1,21 @@
-from django.http import HttpResponse, HttpResponseNotAllowed
-from django.contrib.auth.models import User
-from django.views.decorators.csrf import ensure_csrf_cookie
-import json
-
-from django.contrib.auth import login, logout, authenticate
-
-from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from .models import Article, Comment
-from .serializers import ArticleSerializer, CommentSerializer
+
 
 class PillItemsPerUser(APIView):
+    """
+    Class Class
+    """
     # url:  pill/user/<int:pk>/
     def get(self):
+        """
+        :return: lala
+        """
         # get pill list for user <int:pk>
-        pass
+        return None
 
     def post(self):
+        """
+        :return: lala
+        """
         # add new pill item for user <int:pk>
-        pass
+        return None
