791d7aceb2a1ea2615ae29b4743ae338be2da030
jinsun <jsyoo1117@gmail.com>
2019-11-02 18:25:14 +0900

    Final fix to backed Integration

--- backend/pill/urls.py
+++ backend/pill/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pill/user/<int:pk>/'),
-    path('pill/<int:pk>/')
+    #path('pill/user/<int:pk>/', views.pill, name='vision'),
+    #path('pill/<int:pk>/', views.pill, name='vision'),
 ]
