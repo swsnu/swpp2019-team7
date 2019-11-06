54c9e81e72dc33bd59db6f965c50e2a018f8bead
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-11-06 15:39:06 +0900

    firebase refactored to independent module

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './components/App';
+import Firebase, { FirebaseContext } from "./components/Firebase";
 import store from './store/index';
 import { history } from './store/reducers/index';
 
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App history={history} />
-  </Provider>,
+    <Provider store={store}>
+      <FirebaseContext.Provider value={new Firebase()}>
+        <App history={history} />
+      </FirebaseContext.Provider>
+    </Provider>,
   document.getElementById('root'),
 );
