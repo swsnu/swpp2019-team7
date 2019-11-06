634e1da0236e5188691b63e1478f833ee2705790
peter <peterpan1998@snu.ac.kr>
2019-11-06 14:32:36 +0900

    no message

--- frontend/src/store/reducers/UserReducer.js
+++ frontend/src/store/reducers/UserReducer.js
@@ -6,9 +6,9 @@ const UserReducer = (state = initState, action = null) => {
   if (action == null) return state;
   switch (action.type) {
     case 'SIGNIN_USER':
-      return { ...state, logged_in: action.logged_in };
+      return { ...state, logged_in: true};
     case 'SIGNOUT_USER':
-      return { ...state, logged_in: action.logged_in };
+      return { ...state, logged_in: false };
     case 'SIGNUP_USER':
       return { ...state, logged_in: false };
     default:
