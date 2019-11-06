6de916c57c8a6bdda07d7a1a566a4dbe53445ca4
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-11-06 16:00:34 +0900

    resolved eslint issue

--- frontend/src/components/Firebase/Firebase.js
+++ frontend/src/components/Firebase/Firebase.js
@@ -1,14 +1,14 @@
-import * as firebase from "firebase";
+import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyAzcizSnT2_MJwQOaAF8uJJLnGNkpd1Xv4",
-    authDomain: "pillbox-95f6e.firebaseapp.com",
-    databaseURL: "https://pillbox-95f6e.firebaseio.com",
-    projectId: "pillbox-95f6e",
-    storageBucket: "pillbox-95f6e.appspot.com",
-    messagingSenderId: "2935002363",
-    appId: "1:2935002363:web:64210d4c3b543381e6cc5e",
-    measurementId: "G-72B2GDB01E"
+  apiKey: 'AIzaSyAzcizSnT2_MJwQOaAF8uJJLnGNkpd1Xv4',
+  authDomain: 'pillbox-95f6e.firebaseapp.com',
+  databaseURL: 'https://pillbox-95f6e.firebaseio.com',
+  projectId: 'pillbox-95f6e',
+  storageBucket: 'pillbox-95f6e.appspot.com',
+  messagingSenderId: '2935002363',
+  appId: '1:2935002363:web:64210d4c3b543381e6cc5e',
+  measurementId: 'G-72B2GDB01E',
 };
 
 class Firebase {
@@ -17,18 +17,18 @@ class Firebase {
     firebase.analytics();
 
     const messaging = firebase.messaging();
-    messaging.usePublicVapidKey("BDw_yOOH_N4lbHNd6BAdt1UWdfeYAAShD4Obhzzhr-RNzlXLp4R5-7pkIE-wc2uNiKTlRKpogt7LTCcKjNVEWro");
+    messaging.usePublicVapidKey('BDw_yOOH_N4lbHNd6BAdt1UWdfeYAAShD4Obhzzhr-RNzlXLp4R5-7pkIE-wc2uNiKTlRKpogt7LTCcKjNVEWro');
     this.requestPermission();
   }
 
-  requestPermission() {
+  requestPermission = () => {
     Notification.requestPermission().then((permission) => {
       if (permission === 'granted') {
-        console.log("Notification request permitted.");
+        console.log('Notification request permitted.');
       } else {
-        console.log("Unable to get permission to notify.");
+        console.log('Unable to get permission to notify.');
       }
-      });
+    });
   }
 }
 
