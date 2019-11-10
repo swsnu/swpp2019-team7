7c90624c6ab1827a762c96fd32c83f72b68629bd
Jae Hun Jung <sharkmir1@snu.ac.kr>
2019-11-10 22:06:17 +0900

    refactoring cron.py for PEP-8

--- backend/notification/cron.py
+++ backend/notification/cron.py
@@ -1,8 +1,9 @@
-'''Define the cron job for sending notification'''
+"""Define the cron job for sending notification"""
 from fcm_django.models import FCMDevice
 
-def sendNoti():
-    #TODO: Read in the notification model(not implemented yet), parse the time. 
+
+def send_noti():
+    # TODO: Read in the notification model(not implemented yet), parse the time.
     # This cronjob will be performed every minute. If the parsed time matches current time,
     # send the notification as below.
     # You should use 'filter' and search the device using the device token!
