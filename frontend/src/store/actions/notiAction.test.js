465862493785242ac0d4f4c8d3ecf82a1f54dfb2
peter <peterpan1998@snu.ac.kr>
2019-11-16 17:54:29 +0900

    Added a little bit more test code for notiAction.test

--- frontend/src/store/actions/notiAction.test.js
+++ frontend/src/store/actions/notiAction.test.js
@@ -18,4 +18,16 @@ describe('Noti Action', () => {
     store.dispatch(actionCreators.getWebnoti());
     expect(spy).toHaveBeenCalledTimes(1);
   });
+  it('edit web noti', () => {
+    const spy = jest.spyOn(ax, 'put')
+      .mockImplementation(() => new Promise((resolve) => {
+        const result = {
+          status: 200,
+          data: [],
+        };
+        resolve(result);
+      }));
+    store.dispatch(actionCreators.editWebnoti(1));
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
 });
