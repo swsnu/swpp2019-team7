3c9634ebd968833ab892230641cc228a04c77955
peter <peterpan1998@snu.ac.kr>
2019-11-21 23:28:44 +0900

    Made the time choose prettier

--- frontend/src/containers/Dashboard/NotiSetting/PillNoti.js
+++ frontend/src/containers/Dashboard/NotiSetting/PillNoti.js
@@ -14,6 +14,13 @@ import CheckIcon from '@material-ui/icons/Check';
 import CloseIcon from '@material-ui/icons/Close';
 import * as notiActionCreators from '../../../store/actions/notiAction';
 
+import 'date-fns';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+} from '@material-ui/pickers';
+
 // const useStyles = makeStyles((theme) => ({
 const styles = (theme) => ({
   root: {
@@ -72,68 +79,68 @@ class PillNoti extends Component {
     edit_mode: 0,
     /*
      I assumed that the maximum number of notifications per day are 3 times.
-     0~5 indicate the input field id for changing the time.
-     If there are 3 notification times, there are 2 input fields for each, which are for hour and minute respectively.
+     0~2 indicate the KeyboardTimePicker field id for changing the time.
     */
-    0: (0 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][0].substring(0, 2) : '',
-    1: (0 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][0].substring(2, 4) : '',
-    2: (2 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][1].substring(0, 2) : '',
-    3: (2 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][1].substring(2, 4) : '',
-    4: (4 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][2].substring(0, 2) : '',
-    5: (4 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][2].substring(2, 4) : '',
+    0: (0 < this.props.nmTimes) ? '1998-01-04T' + this.props.pillNotiSetting['time'][0].substring(0, 2) + ':' + this.props.pillNotiSetting['time'][0].substring(2, 4) : '',
+    1: (1 < this.props.nmTimes) ? '1998-01-04T' + this.props.pillNotiSetting['time'][1].substring(0, 2) + ':' + this.props.pillNotiSetting['time'][1].substring(2, 4) : '',
+    2: (2 < this.props.nmTimes) ? '1998-01-04T' + this.props.pillNotiSetting['time'][2].substring(0, 2) + ':' + this.props.pillNotiSetting['time'][2].substring(2, 4) : '',
   }
 
-  //When the user presses the confirm button for changing the schedule
-  passesCredential() {
-    var i;
-    var isValidNum = true;
-    const numReg = /^\d{1,2}$/;
-    for (i = 0; i < this.props.nmTimes * 2; i = i + 2) {
-      if (!numReg.test(this.state[i]) || !numReg.test(this.state[i + 1]) || !(this.state[i] >= 0 && this.state[i] <= 23) || !(this.state[i + 1] >= 0 && this.state[i + 1] <= 59)) {
-        isValidNum = false;
-        break;
-      }
-    }
-    return isValidNum;
-  }
+  onDateChange = id => date => {
+    console.log('this.onDateChange Called! date is ');
+    console.log(String(date));
+    console.log(String(date).substring(16, 21));
+    console.log('index is ' + id);
+    this.setState({
+      [id]: '1998-01-04T' + String(date).substring(16, 21)
+    })
+  };
 
   onConfirm() {
     var timeList = [];
     var i;
-    for (i = 0; i < this.props.nmTimes * 2; i = i + 2) {
-      timeList.push((this.state[i].length === 1 ? '0' + this.state[i] : this.state[i]) + (this.state[i + 1].length === 1 ? '0' + this.state[i + 1] : this.state[i + 1]))
+    for (i = 0; i < this.props.nmTimes; i++) {
+      timeList.push((this.state[i].substring(11, 13) + (this.state[i].substring(14, 16))))
     }
     console.log(timeList);
     //id should be the pill id
     const webNotiItem = { id: this.props.pillNotiSetting['pill-id'], activated: true, time: timeList };
-    if (this.passesCredential()) {
-      this.props.onEditWebNoti(webNotiItem);
-    }
-    else {
-      alert('Hour should be between 00 and 23. Minute should be between 00 and 59')
-      this.setState({
-        0: (0 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][0].substring(0, 2) : '',
-        1: (0 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][0].substring(2, 4) : '',
-        2: (2 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][1].substring(0, 2) : '',
-        3: (2 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][1].substring(2, 4) : '',
-        4: (4 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][2].substring(0, 2) : '',
-        5: (4 < 2 * this.props.nmTimes) ? this.props.pillNotiSetting['time'][2].substring(2, 4) : '',
-      })
-    }
+    this.props.onEditWebNoti(webNotiItem);
   }
 
-  handleInputChange = (e) => {
+  onCancel() {
     this.setState({
-      [e.target.id]: e.target.value
+      0: (0 < this.props.nmTimes) ? '1998-01-04T' + this.props.pillNotiSetting['time'][0].substring(0, 2) + ':' + this.props.pillNotiSetting['time'][0].substring(2, 4) : '',
+      1: (1 < this.props.nmTimes) ? '1998-01-04T' + this.props.pillNotiSetting['time'][1].substring(0, 2) + ':' + this.props.pillNotiSetting['time'][1].substring(2, 4) : '',
+      2: (2 < this.props.nmTimes) ? '1998-01-04T' + this.props.pillNotiSetting['time'][2].substring(0, 2) + ':' + this.props.pillNotiSetting['time'][2].substring(2, 4) : '',
     })
   }
 
   render() {
+
     var inputFieldIndex = 0;
     const { classes } = this.props;
     //Assuming 'time' is a string like 0900, 1200
     const timesList = (this.props.pillNotiSetting['time']).map((time) => (time.substring(0, 2) + ':' + time.substring(2, 4) + " "))
-    const timesInputList = (this.props.pillNotiSetting['time']).map((time) => (<div key={time}><input id={inputFieldIndex++} size='2' value={this.state[inputFieldIndex - 1]} onChange={this.handleInputChange}></input>:<input id={inputFieldIndex++} size='2' value={this.state[inputFieldIndex - 1]} onChange={this.handleInputChange}></input> </div>));
+    //const timesInputList = (this.props.pillNotiSetting['time']).map((time) => (<div key={time}><input id={inputFieldIndex++} size='2' value={this.state[inputFieldIndex - 1]} onChange={this.handleInputChange}></input>:<input id={inputFieldIndex++} size='2' value={this.state[inputFieldIndex - 1]} onChange={this.handleInputChange}></input> </div>));
+    const timesInputList = (this.props.pillNotiSetting['time']).map((time) => (
+      <div key={inputFieldIndex++}>
+        <Grid container justify="space-around">
+          <KeyboardTimePicker
+            margin="normal"
+            id="time-picker"
+            label="Time picker"
+            key={inputFieldIndex - 1}
+            value={this.state[inputFieldIndex - 1]}
+            onChange={this.onDateChange(inputFieldIndex - 1)}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+        </Grid>
+      </div>
+    ))
+    console.log('inputFieldIndex: ' + inputFieldIndex)
     if (this.state.edit_mode === 0) {
       return (
         <div className="Pill">
@@ -190,16 +197,18 @@ class PillNoti extends Component {
                 //className={classes.caption}
                 variant="h5"
               >
-                {timesInputList}
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  {timesInputList}
+                </MuiPickersUtilsProvider>
               </Typography>
             </Grid>
             <Grid item xs={1}>
-              <IconButton id="check-button" aria-label="check" className={classes.margin} onClick={() => {this.onConfirm(); this.setState({edit_mode: 0})}}>
+              <IconButton id="check-button" aria-label="check" className={classes.margin} onClick={() => { this.onConfirm(); this.setState({ edit_mode: 0 }) }}>
                 <CheckIcon fontSize="large" />
               </IconButton>
             </Grid>
             <Grid item xs={1}>
-              <IconButton id="close-button" aria-label="close" className={classes.margin} onClick={() => this.setState({ edit_mode: 0 })}>
+              <IconButton id="close-button" aria-label="close" className={classes.margin} onClick={() => { this.onCancel(); this.setState({ edit_mode: 0 }) }}>
                 <CloseIcon fontSize="large" />
               </IconButton>
             </Grid>
