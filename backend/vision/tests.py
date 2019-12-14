4d8a89636407df0b197b43a9a011205ad7530682
JinSun.Yoo <jsyoo1117@gmail.com>
2019-12-14 19:25:11 +0900

    Travis bugfix

--- backend/vision/tests.py
+++ backend/vision/tests.py
@@ -44,7 +44,7 @@ class VisionTestCase(TestCase):
 
             response = self.client.put('/api/vision/',
                                         {'image_id': 3})
-            self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
             response = self.client.delete('/api/vision/')
