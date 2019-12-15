b3ca37fff44133845cb3c80e616465e9c3b16401
Celine Moon <celine@snu.ac.kr>
2019-12-15 11:40:49 +0900

    prettify demowidget UI

--- frontend/src/containers/Landing/DemoWidget/DemoWidget.js
+++ frontend/src/containers/Landing/DemoWidget/DemoWidget.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -8,7 +9,9 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { Typography, withStyles } from '@material-ui/core';
+import {
+  createMuiTheme, ThemeProvider, Typography, withStyles, Fab,
+} from '@material-ui/core';
 
 import { withRouter } from 'react-router-dom';
 import UploadWidget from '../../../components/UploadWidget/UploadWidget';
@@ -17,18 +20,69 @@ import {
   addUserPill, setImageId, setRenderCustomPill, setNewPill,
 } from '../../../store/actions/pillAction';
 
-const styles = () => ({
+const koreanTheme = createMuiTheme({
+  typography: {
+    fontFamily: "'Nanum Gothic', sans-serif",
+    h2: {
+      fontWeight: 500,
+      fontSize: 55,
+      // fontStyle: "italic"
+    },
+    h3: {
+      fontWeight: 500,
+    },
+    h4: {
+      fontSize: 22,
+      fontWeight: 700,
+    },
+    h5: {
+      fontWeight: 500,
+    },
+    caption: {
+      fontSize: 12,
+    },
+    body1: {
+      color: 'rgba(0, 0, 0, 100)',
+    }
+  },
+});
+
+
+const styles = (theme) => ({
   card: {
+    borderRadius: 30,
     // maxWidth: 335,
     // maxWidth: 345,
   },
   media: {
     maxWidth: 335,
+    borderRadius: 20,
+    marginBottom: 10,
     // height: 0,
     // paddingTop: '56.25%', // 16:9
   },
   cardButtons: {
     justifyContent: 'center',
+    marginBottom: 30,
+  },
+  pillInfo: {
+    borderRadius: 20,
+    marginBottom: 40,
+    padding: theme.spacing(3, 2),
+  },
+  circularButton: {
+    margin: theme.spacing(2),
+    background: '#1480c1',
+    color: 'white',
+    border: '#1480c1',
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+    },
+    padding: 10,
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
   },
 });
 
@@ -108,7 +162,7 @@ class DemoWidget extends Component {
         {this.state.resultModalOpen
           ? (
             // shows parsed results
-            <Box id="resultmodal" align="center">
+            <Box id="resultmodal" align="center" style={{marginBottom: '32%' }}>
               <Card className={classes.card}>
                 {/* <Box maxWidth="335"> */}
                 <CardHeader
@@ -123,41 +177,52 @@ class DemoWidget extends Component {
                 />
                 {this.state.productInfo.productName
                   ? (
-                    <CardContent style={{ maxWidth: 335 }}>
+                    <CardContent style={{ maxWidth: 335, padding: 0 }}>
                       {/* <Box align="left" maxLength="335"> */}
-                      <Grid container alignItems="left">
-                        <Grid item xs={12}>
-                          <Typography align="left" gutterBottom variant="h4" component="h2">
-                            {this.state.productInfo.productName}
-                          </Typography>
+                      <ThemeProvider theme={koreanTheme}>
+                      <Paper elevation={2} className={classes.pillInfo}>
+                        <Grid container alignItems="left">
+                          <Grid item xs={12}>
+                            <Typography align="left" variant="h4" component="h2" style={{ marginLeft: 10, marginBottom: 5 }}>
+                              {this.state.productInfo.productName}
+                            </Typography>
+                          </Grid>
+                          {' '}
+                          <br />
+                          <Grid item xs={12}>
+                            <Typography align="left" variant="caption" display="block" style={{ marginLeft: 10, marginBottom: 20 }}>
+                              {this.state.productInfo.companyName}
+                            </Typography>
+                          </Grid>
+                          <Grid item xs={12} container style={{ marginBottom: 5 }}>
+                            <Grid item xs={2} />
+                            <Grid item xs={3}>
+                              <Typography align="left" variant="body1" style={{ fontWeight: 700 }}>
+                              복용방법:
+                              </Typography>
+                            </Grid>
+                            <Grid item xs={7}>
+                              <Typography align="left" variant="body1" gutterBottom>
+                                {this.state.productInfo.takeMethod}
+                              </Typography>
+                            </Grid>
+                          </Grid>
+                          <Grid item xs={12} container>
+                            <Grid item xs={2} />
+                            <Grid item xs={3}>
+                              <Typography align="left" variant="body1" style={{ fontWeight: 700 }}>
+                              유통기한:
+                              </Typography>
+                            </Grid>
+                            <Grid item xs={7}>
+                              <Typography align="left" variant="body1" gutterBottom>
+                                {this.state.productInfo.expirationDate}
+                              </Typography>
+                            </Grid>
+                          </Grid>
                         </Grid>
-                        {' '}
-                        <br />
-                        <Grid item xs={12}>
-                          <Typography align="left" variant="h6">
-                          제조사:
-                            {' '}
-                            {' '}
-                            {this.state.productInfo.companyName}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Typography align="left" variant="h6" component="p">
-                          복용방법:
-                            {' '}
-                            {' '}
-                            {this.state.productInfo.takeMethod}
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                          <Typography align="left" variant="h6" component="p">
-                          유통기한:
-                            {' '}
-                            {' '}
-                            {this.state.productInfo.expirationDate}
-                          </Typography>
-                        </Grid>
-                      </Grid>
+                      </Paper>
+                    </ThemeProvider>
                       {/* </Box> */}
                     </CardContent>
                   )
@@ -174,10 +239,13 @@ class DemoWidget extends Component {
                     <CardActions className={classes.cardButtons}>
                       {this.props.loggedIn
                         ? (
-                          <Button id="add-new-pill" variant="contained" color="primary" style={{ margin: 16 }} onClick={() => { this.addNewPill(); }}>
-                            {/* <Icon name="checkmark" /> */}
+                          <Fab aria-label="add" className={classes.circularButton} onClick={() => { this.addNewPill(); }}>
                             Save
-                          </Button>
+                          </Fab>
+                          // <Button id="add-new-pill" variant="contained" color="primary" style={{ margin: 16 }} onClick={() => { this.addNewPill(); }}>
+                          //   {/* <Icon name="checkmark" /> */}
+                          //   Save
+                          // </Button>
                         )
                         : (
                           <Button id="login-to-save" variant="contained" color="primary" onClick={() => { this.toggleAcceptPill(); }}>
@@ -185,9 +253,12 @@ class DemoWidget extends Component {
                             Log in to Save
                           </Button>
                         )}
-                      <Button id="retry" variant="contained" color="primary" style={{ margin: 16 }} onClick={() => { this.toggleResultModal(false); }}>
+                      {/*<Button id="retry" variant="contained" color="primary" style={{ margin: 16 }} onClick={() => { this.toggleResultModal(false); }}>*/}
+                      {/*  Retry*/}
+                      {/*</Button>*/}
+                      <Fab id="retry" className={classes.circularButton} onClick={() => { this.toggleResultModal(false); }}>
                         Retry
-                      </Button>
+                      </Fab>
                     </CardActions>
                   )
                   : (
